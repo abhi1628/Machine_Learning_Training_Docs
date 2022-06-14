48a88d3d248d0922baca70b402e2daaa42f7169f
Abhishek Singh <abhishek.ec@global.org.in>
2022-06-14 14:48:21 +0530

    Delete Day 3.3.py

--- Day 3.3.py
+++
@@ -1,8 +0,0 @@
-
-# How do I make my pandas DataFrame smaller and faster?
-import pandas as pd
-data = pd.read_csv('http://bit.ly/kaggletrain')
-data.head()
-
-pd.get_dummies(data.Sex).iloc[:, 1:]
-# How do I create dummy variables in pandas?
