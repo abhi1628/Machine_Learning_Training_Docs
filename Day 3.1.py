da629b46beed91e7dcaa579bc1c9c472e4c9f1c9
Abhishek Singh <abhishek.ec@global.org.in>
2022-06-14 14:48:04 +0530

    Delete Day 3.1.py

--- Day 3.1.py
+++
@@ -1,7 +0,0 @@
-
-# How do I rename columns in a pandas DataFrame?
-# How do I remove columns from a pandas DataFrame?
-# How do I sort a pandas DataFrame or a Series?
-# How do I filter rows of a pandas DataFrame by column value?
-# How do I apply multiple filter criteria to a pandas DataFrame?
-# When should I use the "inplace" parameter in pandas?
